f19802cabff90c1d76429c6d8dca91720ad73324
dat.nguyen <datnguyen231@gmail.com>
2017-02-16 22:38:27 +0700

    All Done

--- PokeFinder/MainVC.swift
+++ PokeFinder/MainVC.swift
@@ -31,6 +31,10 @@ class MainVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
         geoFire = GeoFire(firebaseRef: geoFireRef)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
     func locationAuthStatus() {
 
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
@@ -61,12 +65,64 @@ class MainVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     }
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // this function is called when we add new an annotatino on the map
+        // addAnnotation is called
         var annotaionView: MKAnnotationView?
-        if annotation.isKind(of: MKUserLocation.self) {
+        let annoIdentifier = "Pokemon"
+
+
+        if annotation.isKind(of: MKUserLocation.self) { // annotaion for self
             annotaionView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
             annotaionView?.image = UIImage(named: "ash")
+        } else if let deqAnno = mapView.dequeueReusableAnnotationView(withIdentifier: annoIdentifier) { // annotaion for pokemon
+            // Try to reuse annotation
+            annotaionView = deqAnno
+            annotaionView?.annotation = annotation
+        } else { // annotation default
+            let av = MKAnnotationView(annotation: annotation, reuseIdentifier: annoIdentifier)
+            av.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotaionView = av
         }
+        // Set a button with image for current annotaion if this is a pokemon
+        if let anV = annotaionView, let anno = annotation as? PokeAnnotation {
+            anV.canShowCallout = true
+            anV.image = UIImage(named: "\(anno.pokemonNumber)")
+            let btn = UIButton()
+            btn.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            btn.setImage(UIImage(named: "map"), for: .normal)
+            anV.rightCalloutAccessoryView = btn
+        }
+
+
         return annotaionView
+    }
+    // User change location by swipe change region, re generate pokemon
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        showSightingsOnMap(location: loc)
+    }
+    // Tap the map: show the travelling distance to it
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if let anno = view.annotation as? PokeAnnotation {
+            var place: MKPlacemark!
+            if #available(iOS 10.0, *) {
+                place = MKPlacemark(coordinate: anno.coordinate)
+            } else {
+                // Fallback on earlier versions
+                place = MKPlacemark(coordinate: anno.coordinate, addressDictionary: nil)
+            }
+            let destination = MKMapItem(placemark: place)
+            destination.name = "Pokemon Sighting"
+            let regionDistance: CLLocationDistance = 1000
+            let regionSpan = MKCoordinateRegionMakeWithDistance(anno.coordinate, regionDistance, regionDistance)
+
+            let options = [
+                MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
+                MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span),
+                MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
+            ] as [String : Any]
+            MKMapItem.openMaps(with: [destination], launchOptions: options)
+        }
     }
 
     func createSighting(forLocation location: CLLocation, withPokemon pokeId: Int) {
@@ -74,8 +130,23 @@ class MainVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
         geoFire.setLocation(location, forKey: "\(pokeId)")
     }
 
+    func showSightingsOnMap(location: CLLocation) {
+        // Basically if found location on map with ID we will create a add a annotation on map
+        // Geo fire will go to firebase database and look all data with location and key around our current location
+        let circleQuery = geoFire!.query(at: location, withRadius: 2.5)
+        _ = circleQuery?.observe(GFEventType.keyEntered, with: { (key, location) in
+            if let key = key, let location = location {
+                let anno = PokeAnnotation(coordinate: location.coordinate, pokemonNumber: Int(key)!)
+                self.mapView.addAnnotation(anno)
 
+            }
+        })
+    }
     @IBAction func btnSpotRandomPokemon_OnPressed(_ sender: Any) {
+        // Generate random pokemons
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        let rand = arc4random_uniform(151) + 1 // one of 151
+        createSighting(forLocation: loc, withPokemon: Int(rand))
     }
 
 
